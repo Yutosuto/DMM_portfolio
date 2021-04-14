17fb79613f9e1fce66bfb5f5e80fb2c8936bbd71
Yutosuto <suto.yuto@gmail.com>
2021-04-14 11:51:35 +0000

    fix_validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :favorites
 
   validates :title, presence: true
-  validates :body, presence: true, length: {maxmum: 200}
+  validates :body, presence: true, length: {maximum: 200}
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
